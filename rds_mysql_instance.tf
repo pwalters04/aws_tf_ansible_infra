566162e22992ede0e9e786c74bb1207c5a994edf
pwalters04 <pwalters@honeywell.com>
2019-09-09 13:51:06 -0400

    added 2 addition DB's

--- rds_mysql_instance.tf
+++ rds_mysql_instance.tf
@@ -53,3 +53,45 @@ resource "aws_db_instance" "my_rds_mysql" {
   multi_az                    = true
   skip_final_snapshot         = true
 }
+
+resource "aws_db_instance" "my_rds_mysql_QA" {
+  allocated_storage           = 20
+  storage_type                = "gp2"
+  engine                      = "mysql"
+  engine_version              = "5.7"
+  instance_class              = "${var.rds_instance_class}"
+  name                        = "${var.database_name_QA}"
+  username                    = "${var.database_user}"
+  password                    = "${var.database_temp_password}"
+  parameter_group_name        = "default.mysql5.7"
+  db_subnet_group_name        = "${aws_db_subnet_group.rds-subnet.name}"
+  vpc_security_group_ids      = ["${aws_security_group.rds-security-group.id}"]
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  backup_retention_period     = 35
+  backup_window               = "22:00-23:00"
+  maintenance_window          = "Sat:00:00-Sat:03:00"
+  multi_az                    = true
+  skip_final_snapshot         = true
+}
+
+resource "aws_db_instance" "my_rds_mysql_Prod" {
+  allocated_storage           = 20
+  storage_type                = "gp2"
+  engine                      = "mysql"
+  engine_version              = "5.7"
+  instance_class              = "${var.rds_instance_class}"
+  name                        = "${var.database_name_Prod}"
+  username                    = "${var.database_user}"
+  password                    = "${var.database_temp_password}"
+  parameter_group_name        = "default.mysql5.7"
+  db_subnet_group_name        = "${aws_db_subnet_group.rds-subnet.name}"
+  vpc_security_group_ids      = ["${aws_security_group.rds-security-group.id}"]
+  allow_major_version_upgrade = true
+  auto_minor_version_upgrade  = true
+  backup_retention_period     = 35
+  backup_window               = "22:00-23:00"
+  maintenance_window          = "Sat:00:00-Sat:03:00"
+  multi_az                    = true
+  skip_final_snapshot         = true
+}
